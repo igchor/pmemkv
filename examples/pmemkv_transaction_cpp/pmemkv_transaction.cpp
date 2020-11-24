e46d61ffa587c2f4ff4a3e32dae8c95335ad0868
Igor Chorążewicz <igor.chorazewicz@intel.com>
2020-11-24 12:30:28 +0100

    examples

--- examples/pmemkv_transaction_cpp/pmemkv_transaction.cpp
+++ examples/pmemkv_transaction_cpp/pmemkv_transaction.cpp
@@ -54,10 +54,101 @@ int main(int argc, char *argv[])
 	s = kv.put("key1", "value1");
 	ASSERT(s == status::OK);
 
+	//////////////////////////////////////////////////
+	// Tx with read_for_update
+	auto it1 = kv.new_write_iterator();
 	auto tx = kv.tx_begin();
-	s = tx.remove("key1");
-	s = tx.put("key2", "value2");
-	s = tx.put("key3", "value3");
+
+	it1.seek("k1");
+	auto r1 = it1.read_range();
+	tx.read_for_update(r1);
+
+	it1.seek("k2");
+	auto r2 = it1.read_range();
+	tx.read_for_update(r2);
+
+	it1.seek("k1");	
+	it.write_range()[0] = r1[0];
+	tx.add(it1);
+
+	it1.seek("k2");
+	it.write_range()[0] = r2[0]; // XXX: inconsistent write_range + add vs tx.read_for_update()
+	tx.add(it1);
+
+	tx.commit(); // only succeeds if r1 and r2 didn't change
+	//////////////////////////////////////////////////
+
+	//////////////////////////////////////////////////
+	// Lock multiple elements at once
+	auto it1 = kv.new_write_iterator();
+	auto it2 = kv.new_write_iterator();
+
+	do {
+		auto s = multi_seek(it1, "k1", it, "k2"); // aka SELECT FOR UPDATE, can fail due to conflict (deadlock prevention)
+	while (s != status::CONFLICT);
+
+	auto tx = kv.tx_begin();
+
+	auto r1 = it1.read_range();
+	auto r2 = it2.read_range();
+
+	it2.write_range()[0] = r1[0];
+	it1.write_range()[0] = r2[0];
+
+	tx.add(it1);
+	tx.add(it2);
+	tx.commit();
+	//////////////////////////////////////////////////
+
+	//////////////////////////////////////////////////
+	// seek and commit can return CONFLICT/ABORTED/TIMED_OUT
+	auto it1 = kv.new_write_iterator();
+	auto it2 = kv.new_write_iterator();
+
+	do {
+		auto tx = kv.tx_begin();
+
+		s = it1.seek("k1");
+		s = it2.seek("k2"); // can return LOCK_TIMED_OUT for lock-based engines
+		if (s == status::LOCK_TIMED_OUT)
+			continue;
+
+		auto r1 = it1.read_range();
+		auto r2 = it2.read_range();
+
+		it2.write_range()[0] = r1[0];
+		it1.write_range()[0] = r2[0];
+
+		// it1.commit();
+		// it2.commit();
+		tx.add(it1);
+		tx.add(it2);
+		tx.commit();
+	} while (s == status::LOCK_TIMED_OUT);
+
+	// lock-free engines
+	do {
+		auto tx = kv.tx_begin();
+
+		s = it1.seek("k1");
+		s = it2.seek("k2");
+
+		auto r1 = it1.read_for_update();
+		auto r2 = it2.read_for_update();
+
+		it2.write_range()[0] = r1[0];
+		it1.write_range()[0] = r2[0];
+
+		// it1.commit();
+		// it2.commit();
+		tx.add(it1);
+		tx.add(it2);
+
+		s = tx.commit(); // can return CONFLICT (needs to restart the transaction)
+	} while (s != status::OK);
+	//////////////////////////////////////////////////
+
+	
 
 	/* Until transaction is committed, changes are not visible */
 	ASSERT(kv.exists("key1") == status::OK);
