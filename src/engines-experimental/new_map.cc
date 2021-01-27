5c49fa2f2b0c6aa1f24f65da7a1d202ee2f9a6cd
Igor Chorążewicz <igor.chorazewicz@intel.com>
2021-01-27 10:38:19 +0100

    XXX

--- src/engines-experimental/new_map.cc
+++ src/engines-experimental/new_map.cc
@@ -18,6 +18,10 @@ new_map::new_map(std::unique_ptr<internal::config> cfg)
 
 new_map::~new_map()
 {
+	while (std::atomic_load_explicit(&immutable_map, std::memory_order_acquire) != nullptr) {
+		// XXX: wait on cond var
+	}
+
 	LOG("Stopped ok");
 }
 
@@ -41,46 +45,237 @@ status new_map::count_all(std::size_t &cnt)
 	}, &cnt);
 }
 
+// template <typename Iterator, decltype(std::declval<Iterator>()->first) = {}>
+// static int key(const Iterator& it) {
+// 	return it->first;
+// }
+
+// template <typename Iterator, decltype(std::declval<Iterator>()->key()) = {}>
+// static int key(const Iterator& it) {
+// 	return it->key();
+// }
+
+// template <typename Iterator, decltype(std::declval<Iterator>()->second) = {}>
+// static int value(const Iterator& it) {
+// 	return it->second;
+// }
+
+// template <typename Iterator, decltype(std::declval<Iterator>()->value()) = {}>
+// static int value(const Iterator& it) {
+// 	return it->value();
+// }
+
+template <typename Iterator, typename Pred>
+static status iterate(Iterator first, Iterator last, Pred &&pred, get_kv_callback *callback, void *arg)
+{
+	for (auto it = first; it != last; ++it) {
+		// string_view key = key(it);
+		// string_view value = value(it);
+		string_view key = it->first;
+		string_view value = it->second;
+
+		if (!pred(key, value))
+			continue;
+
+		auto ret =
+			callback(key.data(), key.size(), value.data(), value.size(), arg);
+
+		if (ret != 0)
+			return status::STOPPED_BY_CB;
+	}
+
+	return status::OK;
+}
+
 status new_map::get_all(get_kv_callback *callback, void *arg)
 {
-	LOG("get_all");
+	LOG("get_all");	std::atomic<bool> shutting_down;
 	check_outside_tx();
 
-	auto s = index.iterate(callback, arg);
+	auto mut = std::atomic_load_explicit(&mutable_map, std::memory_order_acquire);
+	auto imm = std::atomic_load_explicit(&immutable_map, std::memory_order_acquire);
+
+	// XXX: can this happen? probably yes
+	if (mut == imm)
+		imm = nullptr;
+
+	auto mut_pred = [](string_view, string_view v) {
+		return (uintptr_t)v.data() != dram_map_type::tombstone;
+	};
+
+	auto imm_pred = [&](string_view k, string_view v) {
+		return !mut->exists(k) && mut_pred(k, v);
+	};
+
+	auto pmem_pred = [&](string_view k, string_view v) {
+		return (!imm || !imm->exists(k)) && !mut->exists(k);
+	};
+
+	auto s = iterate(mut->begin(), mut->end(), mut_pred, callback, arg);
 	if (s != status::OK)
 		return s;
 
-	for (auto &c : *container) {
-		for (auto it = c.cbegin(); it != c.cend(); ++it) {
-			string_view key = it->key();
-			string_view value = it->value();
-
-			if (index.exists(key))
-				continue;
-
-			auto ret =
-				callback(key.data(), key.size(), value.data(), value.size(), arg);
-
-			if (ret != 0)
-				return status::STOPPED_BY_CB;
-		}
+	if (imm) {
+		s = iterate(imm->begin(), imm->end(), imm_pred, callback, arg);
+		if (s != status::OK)
+			return s;
 	}
 
-	return s;
+	// for (auto &c : *container) {
+	// 	s = iterate(c.cbegin(), c.cend(), pmem_pred, callback, arg);
+	// 	if (s != status::OK)
+	// 		return s;
+	// }
+
+	// return s;
+
+		// XXX: should it be above?
+	std::unique_lock<std::shared_timed_mutex> lock(iteration_mtx);
+
+	return iterate(container->begin(), container->end(), pmem_pred, callback, arg);
 }
 
 status new_map::exists(string_view key)
 {
 	LOG("exists for key=" << std::string(key.data(), key.size()));
 
-	return index.exists(key) ? status::OK : status::NOT_FOUND;
+	return get(key, [](const char*, size_t, void*){}, nullptr);
 }
 
 status new_map::get(string_view key, get_v_callback *callback, void *arg)
 {
 	LOG("get key=" << std::string(key.data(), key.size()));
 	
-	return index.get(key, callback, arg) ? status::OK : status::NOT_FOUND;
+	auto mut = std::atomic_load_explicit(&mutable_map, std::memory_order_acquire);
+	auto imm = std::atomic_load_explicit(&immutable_map, std::memory_order_acquire);
+
+	// XXX: think about consistency guarantees with regard to lookups in several maps
+	// Maybe we could use sharding for dram also and then each key would have specific lock
+	// it would also decrease number of locks take (3 -> 1)
+	// if each dram shard corresponds with pmem shard - much simpler compaction
+	// THINK ABOUT SORTED DATASTRUCTURES!!!
+
+	{
+		dram_map_type::const_accessor_type acc;
+		auto s = mut->get(key, acc);
+		if (s == dram_map_type::element_status::alive) {
+			callback(acc->second.data(), acc->second.size(), arg);
+			return status::OK;
+		} else if (s ==  dram_map_type::element_status::removed) {
+			return status::NOT_FOUND;
+		}
+	}
+	
+	{
+		if (imm) {
+			dram_map_type::const_accessor_type acc;
+			auto s = imm->get(key, acc);
+			if (s == dram_map_type::element_status::alive) {
+				callback(acc->second.data(), acc->second.size(), arg);
+				return status::OK;
+			} else if (s ==  dram_map_type::element_status::removed) {
+				return status::NOT_FOUND;
+			}
+		}
+	}
+
+	// unique_lock_type lock(mtxs[shard(key)]);
+
+	// auto it = container->find(key);
+	// if (it != container->end()) {
+	// 	auto value = string_view(it->value());
+	// 	callback(value.data(), value.size(), arg);
+	// 	return status::OK;
+	// }
+
+	container_type::const_accessor result;
+	auto found = container->find(result, key);
+	if (!found)
+		return status::NOT_FOUND;
+
+	callback(result->second.c_str(), result->second.size(), arg);
+
+	return status::OK;
+}
+
+// void radix_compaction()
+// {
+		// 	std::vector<std::vector<index_type::dram_map_type::value_type*>> elements(SHARDS_NUM);
+
+		// for (auto &e : *imm) {
+		// 	elements[shard(e->first)].push_back(&e);
+//}
+
+// 			// // XXX - allocate radix nodes before taking locks (action API?)
+// 		// for (int i = 0; i < SHARDS_NUM; i++) {
+// 		// 	/// XXX: all shards must be saved as a single atomic action (since we don't know the order)
+// 		// 	// use some intermidiate layer? (list of radix nodes?) - we can treat this layer as L0 in LSM-tree probably
+
+// 		// 	unique_lock_type lock(mtxs[i]);
+// 		// 	obj::transaction::run(pmpool, [&]{
+// 		// 		for (const auto &e : elements[i]) {
+// 		// 			if (uintptr_t(e->second.data()) == dram_map_type::tombstone)
+// 		// 				(*container)[i].erase(e->first);
+// 		// 			else	
+// 		// 				(*container)[i].insert_or_assign(e->first, e->second);
+// 		// 		}
+// 		// 	});
+// 		// }
+// }
+
+void new_map::start_bg_compaction(std::shared_ptr<dram_map_type> imm)
+{
+	// XXX: create thread pool
+	auto compaction_thread = std::thread([&, imm=imm]{
+		try {
+		pmem::obj::transaction::run(pmpool, [&]{
+			// XXX: should we ever shrink?
+			// if (log->size() < imm->size())
+			// log->resize(imm->size());
+			assert(insert_log->size() == 0);
+			assert(remove_log->size() == 0);
+
+			insert_log->reserve(imm->size());
+			remove_log->reserve(imm->size());
+
+			for (const auto &e : *imm) {
+				if ((uint64_t) e.second.data() == dram_map_type::tombstone)
+					remove_log->emplace_back(e.first);
+				else
+					insert_log->emplace_back(e.first, e.second);
+			}
+		});
+
+		{
+			std::shared_lock<std::shared_timed_mutex> lock(iteration_mtx);
+			for (const auto &e : *insert_log) {
+				container->insert_or_assign(std::move(e.first), std::move(e.second));
+			}
+
+			for (const auto &e : *remove_log) {
+				container->erase(e);
+			}
+		}
+
+		pmem::obj::transaction::run(pmpool, [&]{
+			insert_log->clear();
+			remove_log->clear();
+		});
+
+		// XXX - move this before cleaning logs - how to solve problem with closing db?
+		std::atomic_store_explicit(&immutable_map, std::shared_ptr<dram_map_type>(nullptr), std::memory_order_release);
+
+		} catch(std::exception &e) {
+			std::cout << e.what() << std::endl;
+		}
+	});
+
+	compaction_thread.detach();
+}
+
+bool new_map::dram_has_space(std::shared_ptr<dram_map_type> map)
+{
+	return map->size() < 1024; // XXX - parameterize
 }
 
 status new_map::put(string_view key, string_view value)
@@ -88,34 +283,35 @@ status new_map::put(string_view key, string_view value)
 	LOG("put key=" << std::string(key.data(), key.size())
 		       << ", value.size=" << std::to_string(value.size()));
 
-	// auto imm = index.maybe_compact();
-	// if (imm) {
-	// 	// XXX: create thread pool,
-	// 	// split the work between several threads (configurable?)
-	// 	auto compaction_thread = std::thread([&]{
-	// 		std::vector<std::vector<index_type::dram_map_type::value_type*>> elements(SHARDS_NUM);
+	auto mut = std::atomic_load_explicit(&mutable_map, std::memory_order_acquire);
 
-	// 		for (auto &e : *imm) {
-	// 			elements[shard(e->first)].push_back(&e);
-	// 		}
+	if (!dram_has_space(mut)) {
+		while (true) {
+			// XXX: to avoid CAS for immutable/mutable map assignments
+			std::unique_lock<std::mutex> lock(compaction_mtx);
+			mut = std::atomic_load_explicit(&mutable_map, std::memory_order_acquire);
 
-	// 		// XXX - allocate radix nodes before taking locks
+			// XXX - what if there is always close right after put?
+			if (dram_has_space(mut)) break;
 
-	// 		for (int i = 0; i < SHARDS_NUM; i++) {
-	// 			unique_lock_type lock(mtxs[i]);
-	// 			obj::transaction::run(pmpool, [&]{
-	// 				for (const auto &e : elements[i])
-	// 					(*container)[i].insert_or_assign(*e);
-	// 			});
-	// 		}
-			
-	// 		index.free_immutable();
-	// 	});
-	// }
+			auto imm = std::atomic_load_explicit(&immutable_map, std::memory_order_acquire);
+			if (imm) {
+				// XXX: wait on cond var
+				//abort();
+				continue;
+			} else {
+				std::atomic_store_explicit(&immutable_map, mutable_map, std::memory_order_release);
+				std::atomic_store_explicit(&mutable_map, std::make_shared<dram_map_type>(), std::memory_order_release);
+
+				start_bg_compaction(mut);
+			}
+		}	
+	}
 
 	// once we reach this point, there might be no space in the dram again...
 	// but if buffer is of reasonable size this should not be a problem (we will exceed the capacity by a small value)
-	index.put(key, value);
+	// we could have custom allocation wrapper - throw exception on eom and restart
+	mut->put(key, value);
 
 	return status::OK;
 }
@@ -124,9 +320,11 @@ status new_map::remove(string_view key)
 {
 	LOG("remove key=" << std::string(key.data(), key.size()));
 
+	auto mut = std::atomic_load_explicit(&mutable_map, std::memory_order_acquire);
+
 	auto found = exists(key);
 
-	index.remove(key);
+	mut->remove(key);
 
 	/* XXX: Should we just always succeed? checking if the element was actually removed could be expensive */
 	// Currently, this is not thread safe
@@ -140,6 +338,8 @@ void new_map::Recover()
 			pmemobj_direct(*root_oid));
 
 		container = &pmem_ptr->map;
+		insert_log = &pmem_ptr->insert_log;
+		remove_log = &pmem_ptr->remove_log;
 	} else {
 		pmem::obj::transaction::run(pmpool, [&] {
 			pmem::obj::transaction::snapshot(root_oid);
@@ -150,12 +350,17 @@ void new_map::Recover()
 				pmemobj_direct(*root_oid));
 
 			container = &pmem_ptr->map;
+			insert_log = &pmem_ptr->insert_log;
+			remove_log = &pmem_ptr->remove_log;
 
-			container->resize(SHARDS_NUM);
+			// container->resize(SHARDS_NUM);
 		});
 	}
 
-	mtxs = std::vector<mutex_type>(SHARDS_NUM);
+	// mtxs = std::vector<mutex_type>(SHARDS_NUM);
+
+	mutable_map = std::make_shared<dram_map_type>();
+	immutable_map = nullptr;
 }
 
 } // namespace kv
--- src/engines-experimental/new_map.h
+++ src/engines-experimental/new_map.h
@@ -11,6 +11,12 @@
 #include <libpmemobj++/experimental/inline_string.hpp>
 #include <libpmemobj++/experimental/radix_tree.hpp>
 
+#include <libpmemobj++/detail/pair.hpp>
+
+#include <libpmemobj++/container/string.hpp>
+
+#include <libpmemobj++/container/concurrent_hash_map.hpp>
+
 #include <tbb/concurrent_hash_map.h>
 
 #include <endian.h>
@@ -24,41 +30,69 @@ namespace internal
 namespace new_map
 {
 
-	template <typename Iterator, typename Pred>
-	status iterate(Iterator first, Iterator last, get_kv_callback *callback, void *arg, Pred &&pred)
+class key_equal {
+public:
+	template <typename M, typename U>
+	bool operator()(const M &lhs, const U &rhs) const
 	{
-		for (auto it = first; it != last; ++it) {
-			string_view key = it->first;
-			string_view value = it->second;
+		return lhs == rhs;
+	}
+};
 
-			if (!pred(key, value))
-				continue;
+class string_hasher {
+	/* hash multiplier used by fibonacci hashing */
+	static const size_t hash_multiplier = 11400714819323198485ULL;
 
-			auto ret =
-				callback(key.data(), key.size(), value.data(), value.size(), arg);
+public:
+	using transparent_key_equal = key_equal;
 
-			if (ret != 0)
-				return status::STOPPED_BY_CB;
-		}
-
-		return status::OK;
+	size_t operator()(const obj::string &str) const
+	{
+		return hash(str.c_str(), str.size());
 	}
 
-using pmem_map_type = pmem::obj::experimental::radix_tree<pmem::obj::experimental::inline_string,
-					    pmem::obj::experimental::inline_string>;
+	size_t operator()(string_view str) const
+	{
+		return hash(str.data(), str.size());
+	}
 
-struct dram_index {
-	using dram_map_type = tbb::concurrent_hash_map<string_view, string_view>;
+private:
+	size_t hash(const char *str, size_t size) const
+	{
+		size_t h = 0;
+		for (size_t i = 0; i < size; ++i) {
+			h = static_cast<size_t>(str[i]) ^ (h * hash_multiplier);
+		}
+		return h;
+	}
+};
 
-	dram_index() {
-		mutable_map = std::make_shared<dram_map_type>();
-		immutable_map = nullptr;
+
+using pmem_map_type = obj::concurrent_hash_map<obj::string, obj::string, string_hasher>;
+
+// using pmem_map_type = pmem::obj::experimental::radix_tree<pmem::obj::experimental::inline_string,
+// 					    pmem::obj::experimental::inline_string>;
+
+using pmem_insert_log_type = obj::vector<detail::pair<obj::string, obj::string>>;
+using pmem_remove_log_type = obj::vector<obj::string>;
+
+struct dram_map_type {
+	using container_type = tbb::concurrent_hash_map<string_view, string_view>;
+	using accessor_type = container_type::accessor;
+	using const_accessor_type = container_type::const_accessor;
+	static constexpr uintptr_t tombstone = std::numeric_limits<uintptr_t>::max();
+
+	~dram_map_type() {
+		for (const auto &e : map) {
+			 delete[] e.first.data();
+
+			 if ((uint64_t) e.second.data() != dram_map_type::tombstone)
+			 	delete[] e.second.data();
+		 }
 	}
 
 	void put(string_view key, string_view value) {
-		auto mutable_map_thr = std::atomic_load_explicit(&mutable_map, std::memory_order_acquire);
-
-		dram_map_type::accessor acc;
+		container_type::accessor acc;
 
 		auto k = new char[key.size()];
 		std::copy(key.begin(), key.data() + key.size(), k);
@@ -66,10 +100,10 @@ struct dram_index {
 		auto v = new char[value.size()];
 		std::copy(value.begin(), value.data() + value.size(), v);
 
-		dram_map_type::value_type kv{string_view(k, key.size()), string_view(v, value.size())};
+		container_type::value_type kv{string_view(k, key.size()), string_view(v, value.size())};
 
 		// XXX - make it exception safe (use C++...)
-		auto inserted = mutable_map_thr->insert(acc, kv);
+		auto inserted = map.insert(acc, kv);
 		if (!inserted) {
 			if ((uintptr_t) acc->second.data() != tombstone)
 				delete[] acc->second.data();
@@ -79,42 +113,37 @@ struct dram_index {
 		}
 	}
 
-	bool get(string_view key, get_v_callback *callback, void *arg) {
-		auto mutable_map_thr = std::atomic_load_explicit(&mutable_map, std::memory_order_acquire);
-		auto immutable_map_thr = std::atomic_load_explicit(&immutable_map, std::memory_order_acquire);
+	enum class element_status {alive, removed, not_found};
 
-		dram_map_type::const_accessor acc;
-		auto found = mutable_map_thr->find(acc, key);
+	element_status get(string_view key, container_type::const_accessor& acc) {
+		auto found = map.find(acc, key);
 
-		if (found && (uintptr_t)acc->second.data() != tombstone) {
-			callback(acc->second.data(), acc->second.size(), arg);
-			return true;
-		}
-		
-		if (immutable_map_thr && immutable_map_thr->find(acc, key) && (uintptr_t)acc->second.data() != tombstone) {
-			callback(acc->second.data(), acc->second.size(), arg);
-			return true;
+		if (found) {
+		 	if ((uintptr_t)acc->second.data() == tombstone)
+				return element_status::removed;
+			else
+				return element_status::alive;
 		}
 
-		return false;
+		return element_status::not_found;
 	}
 
+	/* Element exists in the dram map (alive or tombstone) */
 	bool exists(string_view key)
 	{
-		return get(key, [](const char*, size_t, void*){}, nullptr);
+		return map.count(key) == 0 ? false : true;
 	}
 
 	void remove(string_view key) {
-		auto mutable_map_thr = std::atomic_load_explicit(&mutable_map, std::memory_order_acquire);
-		dram_map_type::accessor acc;
+		container_type::accessor acc;
 
 		auto k = new char[key.size()];
 		std::copy(key.begin(), key.data() + key.size(), k);
 
-		dram_map_type::value_type kv{string_view(k, key.size()), string_view((const char*)tombstone, 0)};
+		container_type::value_type kv{string_view(k, key.size()), string_view((const char*)tombstone, 0)};
 
 		// XXX - make it exception safe (use C++...)
-		auto inserted = mutable_map_thr->insert(acc, kv);
+		auto inserted = map.insert(acc, kv);
 		if (!inserted) {
 			if ((uintptr_t) acc->second.data() != tombstone)
 				delete[] acc->second.data();
@@ -124,66 +153,22 @@ struct dram_index {
 		}
 	}
 
-	status iterate(get_kv_callback *callback, void *arg)
+	container_type::iterator begin()
 	{
-		// no atomic instructions since iterate is not thread safe
-
-		auto mut_pred = [](string_view k, string_view v){return (uintptr_t)v.data() != tombstone;};
-		auto imm_pred = [&](string_view key, string_view v){return mutable_map->count(key) == 0 && (uintptr_t)v.data() != tombstone;};
-
-		auto s = ::pmem::kv::internal::new_map::iterate(mutable_map->begin(), mutable_map->end(), callback, arg, mut_pred);
-		if (s != status::OK)
-			return s;
-
-		if (!immutable_map)
-			return status::OK;
-	
-		return ::pmem::kv::internal::new_map::iterate(immutable_map->begin(), immutable_map->end(), callback, arg, imm_pred);
+		return map.begin();
 	}
 
-	bool has_space()
+	container_type::iterator end()
 	{
-		auto mutable_map_thr = std::atomic_load_explicit(&mutable_map, std::memory_order_acquire);
-
-		/// XXX: count also object sizes?
-		return mutable_map_thr->size() <= dram_capacity;
+		return map.end();
 	}
 
-	std::mutex compaction_mtx;
-	using lock_type = std::unique_lock<std::mutex>;
-
-	// if compaction needed, returns pointer to the immutable_map
-	std::shared_ptr<dram_map_type> maybe_compact()
+	size_t size() const
 	{
-		if (has_space()) return nullptr;
-
-		while(true) {
-			lock_type lock(compaction_mtx);
-
-			if (has_space()) return nullptr;
-
-			if (std::atomic_load_explicit(&immutable_map, std::memory_order_acquire) != nullptr) {
-				// XXX: wait on cond variable
-				while (std::atomic_load_explicit(&immutable_map, std::memory_order_acquire) != nullptr) {}
-			} else {
-				/* Atomic operations required to synchronize with readers. */
-				std::atomic_store_explicit(&immutable_map, mutable_map, std::memory_order_release);
-				std::atomic_store_explicit(&mutable_map, std::make_shared<dram_map_type>(), std::memory_order_release);
-			}
-		}
+		return map.size();
 	}
 
-	void free_immutable()
-	{
-		std::shared_ptr<dram_map_type> tmp = nullptr;
-		std::atomic_store_explicit(&immutable_map, tmp, std::memory_order_release);
-	}
-
-	static constexpr uintptr_t tombstone = std::numeric_limits<uintptr_t>::max();
-	static constexpr uint64_t dram_capacity = 1024; /// XXX: parameterize this
-
-	std::shared_ptr<dram_map_type> mutable_map;
-	std::shared_ptr<dram_map_type> immutable_map;
+	tbb::concurrent_hash_map<string_view, string_view> map;
 };
 
 struct pmem_type {
@@ -192,7 +177,10 @@ struct pmem_type {
 		std::memset(reserved, 0, sizeof(reserved));
 	}
 
-	obj::vector<pmem_map_type> map;
+	//obj::vector<pmem_map_type> map;
+	pmem_map_type map;
+	pmem_insert_log_type insert_log;
+	pmem_remove_log_type remove_log;
 	uint64_t reserved[8];
 };
 
@@ -230,49 +218,64 @@ private:
 
 	static constexpr int SHARDS_NUM = 1024;
 
-	static inline uint64_t
-	mix(uint64_t h)
-	{
-		h ^= h >> 23;
-		h *= 0x2127599bf4325c37ULL;
-		return h ^ h >> 47;
-	}
+	// static inline uint64_t
+	// mix(uint64_t h)
+	// {
+	// 	h ^= h >> 23;
+	// 	h *= 0x2127599bf4325c37ULL;
+	// 	return h ^ h >> 47;
+	// }
 
 
-	uint64_t shard(string_view key) {
-		const uint64_t    m = 0x880355f21e6d1965ULL;
-		const uint64_t *pos = (const uint64_t *)key.data();
-		const uint64_t *end = pos + (key.size() / 8);
-		uint64_t h = key.size() * m;
+	// uint64_t shard(string_view key) {
+	// 	const uint64_t    m = 0x880355f21e6d1965ULL;
+	// 	const uint64_t *pos = (const uint64_t *)key.data();
+	// 	const uint64_t *end = pos + (key.size() / 8);
+	// 	uint64_t h = key.size() * m;
 
-		while (pos != end)
-			h = (h ^ mix(*pos++)) * m;
+	// 	while (pos != end)
+	// 		h = (h ^ mix(*pos++)) * m;
 
-		if (key.size() & 7) {
-			uint64_t shift = (key.size() & 7) * 8;
-			uint64_t mask = (1ULL << shift) - 1;
-			uint64_t v = htole64(*pos) & mask;
-			h = (h ^ mix(v)) * m;
-		}
+	// 	if (key.size() & 7) {
+	// 		uint64_t shift = (key.size() & 7) * 8;
+	// 		uint64_t mask = (1ULL << shift) - 1;
+	// 		uint64_t v = htole64(*pos) & mask;
+	// 		h = (h ^ mix(v)) * m;
+	// 	}
 
-		return mix(h) & (SHARDS_NUM - 1);
-	}
+	// 	return mix(h) & (SHARDS_NUM - 1);
+	// }
 
-	using container_type = obj::vector<internal::new_map::pmem_map_type>;
-	using index_type = internal::new_map::dram_index;
+	///using container_type = obj::vector<internal::new_map::pmem_map_type>;
+	using container_type = internal::new_map::pmem_map_type;
+	using dram_map_type = internal::new_map::dram_map_type;
+	using pmem_insert_log_type = internal::new_map::pmem_insert_log_type;
+	using pmem_remove_log_type = internal::new_map::pmem_remove_log_type;
+
+	void start_bg_compaction(std::shared_ptr<dram_map_type> imm);
+	bool dram_has_space(std::shared_ptr<dram_map_type> map);
 
 	void Recover();
 
 	container_type *container;
+	pmem_insert_log_type *insert_log;
+	pmem_remove_log_type *remove_log;
 	std::unique_ptr<internal::config> config;
-
-	index_type index;
 
 	using mutex_type = std::shared_timed_mutex;
 	using unique_lock_type = std::unique_lock<mutex_type>;
 	using shared_lock_type = std::shared_lock<mutex_type>;
 
-	std::vector<mutex_type> mtxs;
+	// std::vector<mutex_type> mtxs;
+
+	static constexpr uint64_t dram_capacity = 1024; /// XXX: parameterize this
+
+	std::shared_ptr<dram_map_type> mutable_map;
+	std::shared_ptr<dram_map_type> immutable_map;
+
+	std::mutex compaction_mtx;
+
+	std::shared_timed_mutex iteration_mtx;
 };
 
 } /* namespace kv */
