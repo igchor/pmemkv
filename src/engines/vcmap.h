03ffdf6d7c2e40989b74ffa5b6efdaeb2a247537
Igor Chorążewicz <igor.chorazewicz@intel.com>
2021-02-08 14:16:21 +0100

    hetero find/insert

--- src/engines/vcmap.h
+++ src/engines/vcmap.h
@@ -4,21 +4,35 @@
 #pragma once
 
 #include "../engine.h"
-#include "pmem_allocator.h"
 #include <scoped_allocator>
 #include <string>
 #include <tbb/concurrent_hash_map.h>
-
-#ifdef USE_LIBMEMKIND_NAMESPACE
-namespace memkind_ns = libmemkind::pmem;
-#else
-namespace memkind_ns = pmem;
-#endif
 
 namespace pmem
 {
 namespace kv
 {
+
+class string_hasher {
+public:
+	using is_transparent = void;
+
+	size_t hash(const std::string &str) const
+	{
+		return std::hash<std::string>{}(str);
+	}
+
+	size_t hash(string_view str) const
+	{
+		return std::hash<string_view>{}(str);
+	}
+
+	template <typename M, typename U>
+	bool equal(const M &lhs, const U &rhs) const
+	{
+		return lhs == rhs;
+	}
+};
 
 class vcmap : public engine_base {
 	template <bool IsConst>
@@ -46,26 +60,17 @@ public:
 	internal::iterator_base *new_const_iterator() final;
 
 private:
-	typedef memkind_ns::allocator<char> ch_allocator_t;
-	typedef std::basic_string<char, std::char_traits<char>, ch_allocator_t>
-		pmem_string;
-	typedef memkind_ns::allocator<std::pair<pmem_string, pmem_string>> kv_allocator_t;
-	typedef tbb::concurrent_hash_map<pmem_string, pmem_string,
-					 tbb::tbb_hash_compare<pmem_string>,
-					 std::scoped_allocator_adaptor<kv_allocator_t>>
-		map_t;
-	kv_allocator_t kv_allocator;
-	ch_allocator_t ch_allocator;
+	typedef std::basic_string<char> pmem_string;
+	typedef tbb::concurrent_hash_map<pmem_string, pmem_string, string_hasher> map_t;
 	map_t pmem_kv_container;
 };
 
 template <>
 class vcmap::vcmap_iterator<true> : virtual public internal::iterator_base {
 	using container_type = vcmap::map_t;
-	using ch_allocator_t = memkind_ns::allocator<char>;
 
 public:
-	vcmap_iterator(container_type *container, ch_allocator_t *ca);
+	vcmap_iterator(container_type *container);
 
 	status seek(string_view key) final;
 
@@ -76,16 +81,14 @@ public:
 protected:
 	container_type *container;
 	container_type::accessor acc_;
-	ch_allocator_t *ch_allocator;
 };
 
 template <>
 class vcmap::vcmap_iterator<false> : public vcmap::vcmap_iterator<true> {
 	using container_type = vcmap::map_t;
-	using ch_allocator_t = memkind_ns::allocator<char>;
 
 public:
-	vcmap_iterator(container_type *container, ch_allocator_t *ca);
+	vcmap_iterator(container_type *container);
 
 	result<pmem::obj::slice<char *>> write_range(size_t pos, size_t n) final;
 	status commit() final;
